dfd27e5292604cb89bae2c2463667a87270f6672
daniel.zywalewski <daniel.zywalewski@britenet.com.pl>
2021-09-27 14:39:11 +0200

    ora

--- src/main/java/com/pluralsight/order/dao/InsertOrderDao.java
+++ src/main/java/com/pluralsight/order/dao/InsertOrderDao.java
@@ -4,6 +4,7 @@ import com.pluralsight.order.dto.OrderDto;
 import com.pluralsight.order.dto.OrderDetailDto;
 import com.pluralsight.order.util.Database;
 import com.pluralsight.order.util.ExceptionHandler;
+import com.pluralsight.order.util.OrderStatus;
 
 import java.sql.*;
 
@@ -36,24 +37,27 @@ public class InsertOrderDao {
     public long insertOrder(OrderDto orderDto) {
         long orderId = -1;
 
-        try (Connection con = null;
+        try (Connection con = database.getConnection();
              PreparedStatement ps = createOrderPreparedStatement(con, orderDto)
         ) {
+            con.setAutoCommit(false);
 
 
 
-            try (ResultSet result = null) {
+            try (ResultSet result = ps.getGeneratedKeys()) {
                 if(result != null) {
                     if(!result.next()){
+                        con.rollback();
 
                     } else {
-
+                        orderId=result.getLong("order_id");
 
                         for (OrderDetailDto orderDetailDto : orderDto.getOrderDetail()) {
                             orderDetailDto.setOrderId(orderId);
 
                             try (PreparedStatement detailsPS =
                                          createOrderDetailPreparedStatement(con, orderDetailDto)) {
+                                detailsPS.executeUpdate();
 
                             }
                         }
@@ -79,8 +83,11 @@ public class InsertOrderDao {
      * @throws SQLException In case of an error
      */
     private PreparedStatement createOrderPreparedStatement(Connection con, OrderDto orderDto) throws SQLException {
-
-        return null;
+        PreparedStatement preparedStatement = con.prepareStatement(sqlOrder,Statement.RETURN_GENERATED_KEYS);
+        preparedStatement.setLong(1,orderDto.getCustomerId());
+        preparedStatement.setTimestamp(2, (Timestamp) orderDto.getDate());
+        preparedStatement.setString(3, String.valueOf(OrderStatus.CREATED));
+        return preparedStatement;
     }
 
     /**
@@ -91,7 +98,10 @@ public class InsertOrderDao {
      * @throws SQLException In case of an error
      */
     private PreparedStatement createOrderDetailPreparedStatement(Connection con, OrderDetailDto orderDetailDto) throws SQLException {
-
-        return null;
+        PreparedStatement preparedStatement = con.prepareStatement(sqlOrder,Statement.RETURN_GENERATED_KEYS);
+        preparedStatement.setLong(1,orderDetailDto.getOrderId());
+        preparedStatement.setLong(2, orderDetailDto.getProductId());
+        preparedStatement.setInt(3,orderDetailDto.getQuantity());
+        return preparedStatement;
     }
 }
